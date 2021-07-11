f199e94077dc8afef0e0eed0be430503e204b511
Susanna Carey <susannaopal@gmail.com>
2021-07-11 15:52:34 -0400

    Update tests in Library

--- src/Library.js
+++ src/Library.js
@@ -15,28 +15,30 @@ function createLibrary(name) {
 
 
 
-function addBook(name, book) {
-  // var library = createLibrary();
-  // var library = {
-  //   name: createLibrary(),
-    // shelves: {
-    //   fantasy: [],
-    //   fiction: [],
-    //   nonFiction: [],
-    // }
-    // library.shelves.genre.title
-
-
-   // return name.shelves.fantasy.unshift(book);
-   return name.shelves[book.genre].unshift(book);
-
+function addBook(libraryName, book) {
+   return libraryName.shelves[book.genre].unshift(book);
 }
 
 
 
 
+function checkoutBook(libraryName, title, genreShelf) {
+  for (var i = 0; i < libraryName.shelves[genreShelf].length; i++) {
+    if (title === libraryName.shelves[genreShelf][i].title) {
+      libraryName.shelves[genreShelf].splice(i, 1);
+        return `You have now checked out ${title} from the ${libraryName.name}`;
+
+      }
+    //library shelves genre book
+  }
+  // return libraryName.shelves[genreShelf].splice();
+
+}
+ //what kind of data types they are and
+ // how they are nested
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
