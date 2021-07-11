a80d4d94f6413970507080b65ebb40ccfc4b8361
Susanna Carey <susannaopal@gmail.com>
2021-07-11 11:39:45 -0400

    Add update to shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -28,32 +28,15 @@ function listTitles(shelf) {
 
 
 
-function searchShelf (bookShelf) {
-  var nonFictionTitles = [];
+function searchShelf (bookShelf, title) {
   for (var i = 0; i < bookShelf.length; i++) {
-    titles.push(bookShelf[i].title);
+    if (bookShelf[i].title === title) {
+    return true;
+    }
   }
-  return nonFictionTitles;
-}
+  return false;
+  }
 
-
-// function searchShelf(bookShelf) {
-//   var nonFictionTitles = [];
-//   for (var i = 0; i < bookShelf.lenth; i++) {
-//     if (titles[i].number > 1) {
-//       return true;
-//     }
-//     // else false;
-//   }
-// }
-
-// for (var i = 0; i < arrayToCheck.length; i++) {
-//   if (arrayToCheck[i].number > 1) {
-//     return true;
-//   }
-// }
-//
-// return false;
 
 
 module.exports = {
